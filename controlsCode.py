488b3feaecdb24a13d17ae1c603773b8069d7804
osman <oelgh031@uottawa.ca>
2021-03-04 09:22:55 -0500

    Another commit

--- controlsCode.py
+++ controlsCode.py
@@ -26,15 +26,46 @@ def extract_data(extension):
     raw_data = np.char.strip(raw_data, ';')
     raw_data = np.char.strip(raw_data, ']')
     return raw_data
+
+def plot_data(fig_number, set_name, x, y1, y2):
+    fig = plt.figure(fig_number)
+    plt.plot(x, y1)
+    plt.plot(x, y2)
+    plt.legend(['Commanded Position', 'Encoded Position'])
+    plt.xlabel('Time')
+    plt.ylabel('Position')
+    plt.savefig(set_name + '.png')
+    fig.show()
                 
+#Processing the required datasets
 def processing_Datasets():
+    global figure_counter
     set_names = np.array(['frequency response critically damped', 
-                           'frequency response over damped',
-                           'frequency response under damped',
-                           'P response Kp=2X',
-                           'P response Kp=X',
-                           'PD response critically damped',
-                           'PD response over damped',
-                           'PD response under damped',
-                           'PID response Ki=2X',
-                           'PID response Ki=X'])
+                          'frequency response over damped',
+                          'frequency response under damped',
+                          'P response Kp=2X',
+                          'P response Kp=X',
+                          'PD response critically damped',
+                          'PD response over damped',
+                          'PD response under damped',
+                          'PID response Ki=2X',
+                          'PID response Ki=X'])
+
+    for i in range(np.size(set_names)):
+        data = extract_data(set_names[i])
+
+        time   = data[:, 1]
+        posCom = data[:, 2]
+        posEnc = data[:, 3]
+        figure_counter += 1
+
+        plot_data(figure_counter, set_names[i], time, posCom, posEnc)
+    
+    input()
+
+    return set_names, data
+
+set_names, data = processing_Datasets()
+
+
+
